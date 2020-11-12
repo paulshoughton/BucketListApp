5a7a5a9951e9e71faf77426dc3569a7e1618d136
Paul Houghton <paul.houghton@gmail.com>
2020-11-12 08:30:54 +0000

    Initial Commit

---
+++ BucketListApp/BucketListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListAppApp.swift
+//  BucketListApp
+//
+//  Created by Paul Houghton on 12/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct BucketListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
